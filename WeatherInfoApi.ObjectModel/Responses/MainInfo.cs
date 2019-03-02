e3660f85b8ff1e1d569fcee0f0061b7465bf2cd6
Kire <kirebosev@gmail.com>
2019-03-02 19:26:56 +0100

    Bug fix

--- WeatherInfoApi.ObjectModel/Responses/MainInfo.cs
+++ WeatherInfoApi.ObjectModel/Responses/MainInfo.cs
@@ -3,9 +3,11 @@
     public class MainInfo
     {
         public double temp { get; set; }
-        public int pressure { get; set; }
-        public int humidity { get; set; }
+        public double pressure { get; set; }
+        public double humidity { get; set; }
         public double temp_min { get; set; }
         public double temp_max { get; set; }
-    }
+        public double sea_level { get; set; }
+        public double grnd_level { get; set; }                          
+    }                 
 }
